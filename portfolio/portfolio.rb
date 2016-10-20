583f2628d16aaa09e556ada940fe2551fede47d8
John Christie <omeara.christie@gmail.com>
2016-10-20 15:16:03 -0400

    Update ENV

--- portfolio/portfolio.rb
+++ portfolio/portfolio.rb
@@ -8,7 +8,7 @@ end
 post '/contact' do
 	#params.to_s
 
-	Pony.mail :to =>'',
+	Pony.mail :to => ENV["email"],
 			  :from => 'portfolio',
 			  :subject => params[:subject],
 			  :body => "fn: #{params[:first_name]}; ln: #{params[:last_name]}; email: #{params[:email]}; message: #{params[:message]}"
