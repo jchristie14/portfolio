480f4cf0eafd7756979f1ce021220aac8a54ca3d
John Christie <omeara.christie@gmail.com>
2016-10-21 11:05:45 -0400

    Add sendgrid

--- portfolio/portfolio.rb
+++ portfolio/portfolio.rb
@@ -1,15 +1,34 @@
 require 'sinatra'
 require 'pony'
+require 'sendgrid-ruby'
+include SendGrid
 
 get '/' do
   erb :index
 end
 
-post '/contact' do
-	Pony.mail :to => '',
-			  :from => 'portfolio',
-			  :subject => params[:subject],
-			  :body => "fn: #{params[:first_name]}; ln: #{params[:last_name]}; email: #{params[:email]}; message: #{params[:message]}"
+# post '/contact' do
+# 	Pony.mail :to => '',
+# 			  :from => 'portfolio',
+# 			  :subject => params[:subject],
+# 			  :body => "fn: #{params[:first_name]}; ln: #{params[:last_name]}; email: #{params[:email]}; message: #{params[:message]}"
 
-	redirect back
+# 	redirect back
+# end
+
+post '/contact' do
+
+  from = Email.new(email: 'portfolio')
+  subject = 'Hello World from the SendGrid Ruby Library!'
+  to = Email.new(email: 'omeara.christie@gmail.com')
+  content = Content.new(type: 'text/plain', value: "#{params[:message]}")
+  mail = Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts response.status_code
+  puts response.body
+  puts response.headers
+
 end
+
